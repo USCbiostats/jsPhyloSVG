aad75ba9645052a98f3daaf564dd49683d1a16c6
George G. Vega Yon <g.vegayon@gmail.com>
2017-12-14 16:56:45 -0800

    Adding website

---
+++ docs/index_files/jsPhyloSVG-binding-0.0.9000/jsPhyloSVG.js
@@ -0,0 +1,37 @@
+HTMLWidgets.widget({
+
+  name: 'jsPhyloSVG',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+
+        // TODO: code to render the widget, e.g.
+        // window.onload = function(){
+        	YUI().use('oop', 'json-stringify', 'io-base', 'event', 'event-delegate', function(Y){
+        	  phylocanvas = new Smits.PhyloCanvas(
+        	    {newick: x.tree},
+        	    el.id,
+        	    width, height,
+        	    x.layout
+        	    );
+        	});
+        //};
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
